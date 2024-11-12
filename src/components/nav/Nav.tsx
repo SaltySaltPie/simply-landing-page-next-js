e7c435b940c5fec67a9f6c8579462858f79ce6f8
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 09:43:06 +0700

    feat: add hook to get nav items

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -3,26 +3,34 @@ import { useContent } from "@/hooks/useContent";
 import styles from "./Nav.module.scss";
 import Link from "next/link";
 export const Nav = () => {
-   const { content } = useContent();
+   const { navItems } = useNavItems();
 
    return (
       <nav className={`${styles.contentC}`}>
          <div className={`${styles.logoC}`}></div>
-         <div className={`${styles.navItems}`}>
-            <Link href="/">Home</Link>
-            {content["business"] && <Link href="#business">Our Business</Link>}
-            {content["faqs"] && <Link href="#faqs">FAQs</Link>}
-            {content["properties"] && <Link href="#properties">Purchased Properties</Link>}
-            {content["process"] && <Link href="#process">Our Process</Link>}
-            {content["about"] && (
-               <Link href={content["about"].link} target="_blank" rel="noreferrer noopener">
-                  About Simply Homes
-               </Link>
-            )}
-         </div>
+         <ul className={`${styles.navItems}`}>
+            {navItems.map((item) => (
+               <li key={item.title}>
+                  <Link href={item.href}>{item.title}</Link>
+               </li>
+            ))}
+         </ul>
          <div className={`${styles.phone}`}>
             <Link href="tel:2073006144">(207) 300-6144</Link>
          </div>
+         <div className={`${styles.hamburger}`}></div>
       </nav>
    );
+};
+
+const useNavItems = () => {
+   const { content } = useContent();
+
+   const navItems: { title: string; href: string }[] = [{ title: "Home", href: "/" }];
+   if (content["business"]) navItems.push({ title: "Our Business", href: "#business" });
+   if (content["faqs"]) navItems.push({ title: "FAQs", href: "#faqs" });
+   if (content["properties"]) navItems.push({ title: "Purchased Properties", href: "#properties" });
+   if (content["process"]) navItems.push({ title: "Our Process", href: "#process" });
+   if (content["about"]) navItems.push({ title: "About Simply Homes", href: content["about"].link });
+   return { navItems };
 };
