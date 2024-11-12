71bd572d7518250334c18bf720af530bea9e2f5f
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 14:14:51 +0700

    feat: footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,4 +1,13 @@
+import Image from "next/image";
 import styles from "./Footer.module.scss";
+import { CF_CDN_URL } from "@/utils/CF_CDN_URL";
 export const Footer = () => {
-   return <footer className={`${styles.contentC}`}>$1</footer>;
+   return (
+      <footer className={`${styles.contentC}`}>
+         <div className={`${styles.logoC}`}>
+            <Image alt="logo" src={CF_CDN_URL("/assets/common/simply-logo.svg")} fill />
+         </div>
+         <span>© 2024 Simply Homes. All rights reserved.</span>
+      </footer>
+   );
 };
