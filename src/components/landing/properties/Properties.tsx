ead5982032a019fbe8d74c8a7effb5e99855d509
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 15:46:28 +0700

    feat: add sizes

--- src/components/landing/properties/Properties.tsx
+++ src/components/landing/properties/Properties.tsx
@@ -1,11 +1,18 @@
-import styles from './Properties.module.scss'
-export const Properties = ({}:TPropertiesProps) => {
-  return (
-    <div className={`${styles.contentC}`}>
-       $1
-    </div>
-  );
+import { useContent } from "@/hooks/useContent";
+import styles from "./Properties.module.scss";
+import Carousel from "react-multi-carousel";
+export const Properties = () => {
+   const { properties } = useContent().content;
+   if (!properties) return null;
+   const { title, locations } = properties;
+   const a = {};
+   return (
+      <div className={`${styles.contentC}`}>
+         <h2>{title}</h2>
+         <Carousel responsive={a} showDots>
+            <div>a</div>
+            <div>b</div>
+         </Carousel>
+      </div>
+   );
 };
-
-type TPropertiesProps = {
-}
