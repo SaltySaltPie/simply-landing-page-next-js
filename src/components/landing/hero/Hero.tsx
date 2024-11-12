83895133e01347772a72855708246de3f144f5f6
ImOcean <nguyenngocnhan2104@gmail.com>
2024-11-12 16:27:52 +0700

    wip: tweak UI hero

--- src/components/landing/hero/Hero.tsx
+++ src/components/landing/hero/Hero.tsx
@@ -2,33 +2,30 @@
 
 import { useContent } from "@/hooks/useContent";
 import styles from "./Hero.module.scss";
-import { CF_CDN_URL } from "@/utils/CF_CDN_URL";
 import Image from "next/image";
 import { icons } from "@/utils/icon";
-// import { icons } from "@/utils/icon";
 
 export const Hero = () => {
    const { content } = useContent();
 
    const { hero } = content;
+
    if (!hero) return null;
 
    return (
       <div className={`${styles.contentC}`}>
-         <div
-            className={`${styles.heroC}`}
-            style={{
-               background: `url(${CF_CDN_URL("/assets/home_bg.png")}) lightgray 50% / cover no-repeat`,
-            }}
-         >
+         <div className={`${styles.heroC}`}>
             <div className={`${styles.title}`}>
                <h4>{content.hero.subtitle}</h4>
                <h1>{content.hero.title}</h1>
                <div className={`${styles.searchC}`}>
                   <input type="text" placeholder="Enter your property address" />
                   <div className={`${styles.searchIcon}`}>
-                     <Image src={icons.header.arrow_forward.trimEnd()} alt="" width={20} height={20} />
+                     <Image src={icons.header.arrow_forward} alt="" fill />
                   </div>
+               </div>
+               <div className={`${styles.iconAgency}`}>
+                  <Image src={icons.header.hero.bigIcon["bbb"]} alt="" fill objectFit="cover" />
                </div>
             </div>
          </div>
