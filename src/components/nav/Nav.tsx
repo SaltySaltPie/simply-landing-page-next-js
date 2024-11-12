bbb108cbf6290e418cbe30e6cff13ba5a9e22e81
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 08:42:17 +0700

    feat: nav

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,5 +1,40 @@
+"use client";
+import { useContent } from "@/hooks/useContent";
 import styles from "./Nav.module.scss";
+import Link from "next/link";
 export const Nav = () => {
-   return <div className={`${styles.contentC}`}>$1</div>;
-};
+   const { content } = useContent();
 
+   return (
+      <nav className={`${styles.contentC}`}>
+         <Link href="/">
+            <a>Home</a>
+         </Link>
+         {content["business"] && (
+            <Link href="#business">
+               <a>Our Business</a>
+            </Link>
+         )}
+         {content["faqs"] && (
+            <Link href="#faqs">
+               <a>FAQs</a>
+            </Link>
+         )}
+         {content["properties"] && (
+            <Link href="#properties">
+               <a>Purchased Properties</a>
+            </Link>
+         )}
+         {content["process"] && (
+            <Link href="#process">
+               <a>Our Process</a>
+            </Link>
+         )}
+         {content["about"] && (
+            <Link href={content["about"].link} target="_blank" rel="noreferrer noopener">
+               <a>About Simply Homes</a>
+            </Link>
+         )}
+      </nav>
+   );
+};
