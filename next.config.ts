529f4b8a0d0e72f97a5bc31c9d6554fb68fa3831
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 13:58:17 +0700

    feat: allow cf cdn

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   /* config options here */
+   images: {
+      remotePatterns: [
+         {
+            // https://dyaj6jwzih5dm.cloudfront.net
+            protocol: "https",
+            hostname: "dyaj6jwzih5dm.cloudfront.net",
+            pathname: "/**",
+         },
+      ],
+   },
 };
 
 export default nextConfig;
