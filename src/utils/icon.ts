99975537c4002cd0ae608cf754e62825ef5e81cf
ImOcean <nguyenngocnhan2104@gmail.com>
2024-11-12 14:26:30 +0700

    wip: add more icons

--- src/utils/icon.ts
+++ src/utils/icon.ts
@@ -8,7 +8,6 @@ export const icons = {
       close: CF_CDN_URL("/assets/header/close_black.svg"),
    },
    header: {
-      open_in_new: CF_CDN_URL("/assets/header/phone_black.svg"),
       home_bg: CF_CDN_URL("/assets/home_bg.png"),
       arrow_forward: CF_CDN_URL("/assets/header/arrow_forward_black.svg"),
       hero: {
@@ -28,6 +27,28 @@ export const icons = {
             techCrunch: CF_CDN_URL("/assets/header/hero/smallIcon/TechCrunch.png"),
             trustPilot: CF_CDN_URL("/assets/header/hero/smallIcon/trustPilot.png"),
          },
+         open_in_new: CF_CDN_URL("/assets/header/phone_black.svg"),
       },
+   },
+   business: {
+      soldSimply: CF_CDN_URL("/assets/business/soldsimply.png"),
+   },
+   faqs: {
+      add: CF_CDN_URL("/assets/faqs/add_black.svg"),
+      remove: CF_CDN_URL("/assets/faqs/remove_black.svg"),
+      viewMore: CF_CDN_URL("/assets/faqs/keyboard_double_arrow_down_black.svg"),
+   },
+   properties: {
+      location: CF_CDN_URL("/assets/property/location_on_black.svg"),
+      property1: CF_CDN_URL("/assets/property/property1.jpeg"),
+      property2: CF_CDN_URL("/assets/property/property2.jpeg"),
+      property3: CF_CDN_URL("/assets/property/property3.jpeg"),
+   },
+   process: {
+      process1: CF_CDN_URL("/assets/process/process1.png"),
+      process2: CF_CDN_URL("/assets/process/process2.png"),
+      process3: CF_CDN_URL("/assets/process/process3.png"),
+      process4: CF_CDN_URL("/assets/process/process4.png"),
+      process5: CF_CDN_URL("/assets/process/process5.png"),
    },
 };
