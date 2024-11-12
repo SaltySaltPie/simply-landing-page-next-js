d182358a0444fd82059d0564d3a635c2f0242bb2
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 18:24:45 +0700

    fix: build

--- src/components/common/mainButton/MainButton.tsx
+++ src/components/common/mainButton/MainButton.tsx
@@ -1,11 +1,4 @@
-import styles from './MainButton.module.scss'
-export const MainButton = ({}:TMainButtonProps) => {
-  return (
-    <div className={`${styles.contentC}`}>
-       $1
-    </div>
-  );
+import styles from "./MainButton.module.scss";
+export const MainButton = () => {
+   return <div className={`${styles.contentC}`}>$1</div>;
 };
-
-type TMainButtonProps = {
-}
