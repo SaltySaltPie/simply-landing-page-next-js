2f687634b8ce1c47bfc07d4ce9659526133177b1
SaltiestSaltySaltPie <84831252+SaltySaltPie@users.noreply.github.com>
2024-11-12 14:48:48 +0700

    feat: about section

--- src/components/landing/about/About.tsx
+++ src/components/landing/about/About.tsx
@@ -1,11 +1,25 @@
-import styles from './About.module.scss'
-export const About = ({}:TAboutProps) => {
-  return (
-    <div className={`${styles.contentC}`}>
-       $1
-    </div>
-  );
+import { useContent } from "@/hooks/useContent";
+import styles from "./About.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import { CF_CDN_URL } from "@/utils/CF_CDN_URL";
+export const About = () => {
+   const { content } = useContent();
+   const { about } = content;
+   if (!about) return null;
+   return (
+      <div className={`${styles.contentC}`}>
+         <div>
+            <div className={`${styles.content}`}>
+               {about?.title && <h2>{about.title}</h2>}
+               <Link href={about.link}>
+                  <h5>About Us</h5>
+               </Link>
+            </div>
+            <div className={`${styles.banner}`}>
+               <Image alt="logo" src={CF_CDN_URL("/assets/about/banner.svg")} fill layout="raw" />
+            </div>
+         </div>
+      </div>
+   );
 };
-
-type TAboutProps = {
-}
