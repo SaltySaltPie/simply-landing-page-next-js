e4e7069f45d64041a2cc3f2822941902bb8cbb40
KhanhNgocNe <khanhngoc04032000@gmail.com>
2024-12-18 17:32:25 +0700

    wip: update Comparison we renovate

--- src/components/renovate/WeRenoProperties/WeRenoProperties.tsx
+++ src/components/renovate/WeRenoProperties/WeRenoProperties.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useContent } from "@/hooks/useContent";
 import styles from "./WeRenoProperties.module.scss";
-import { ResponsiveType } from "react-multi-carousel";
+import Carousel, { ResponsiveType } from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import Image from "next/image";
 import { icons } from "@/utils/icon";
@@ -9,7 +9,7 @@ import { CustomCarousel } from "@/components/common/customCarousel/CustomCarouse
 export const WeRenoProperties = () => {
    const { properties } = useContent().content;
    if (!properties) return null;
-   const { title, locations } = properties;
+   // const { title, locations } = properties;
 
    return (
       <div className={`${styles.contentC}`} id="properties">
@@ -20,20 +20,32 @@ export const WeRenoProperties = () => {
             arrows
             removeArrowOnDeviceType={["tablet", "mobile"]}
             showDots
-            autoPlay
+            // autoPlay
             infinite
          >
             {locations.map(({ location, photo }, index) => (
                <div key={index} className={`${styles.property}`}>
-                  <div className={`${styles.photo}`}>
-                     <Image alt={location} src={photo} fill sizes="33vw" />
-                  </div>
+                  <Carousel
+                     responsive={responsives}
+                     arrows={false}
+                     showDots
+                     infinite
+                     containerClass={`${styles.compare}`}
+                  >
+                     <div className={`${styles.photo}`}>
+                        <Image alt={location} src={photo.before} fill sizes="33vw" />
+                     </div>
+                     <div className={`${styles.photo}`}>
+                        <Image alt={location} src={photo.after} fill sizes="33vw" />
+                     </div>
+                  </Carousel>
                   <div className={`${styles.location}`}>
                      <div className={`${styles.pin}`}>
                         <Image alt="pin" src={icons.properties["location"]} fill sizes="10vw" />
                      </div>
                      <h3>{location}</h3>
                   </div>
+                  <span>Explain briefly exceeded standards</span>
                </div>
             ))}
          </CustomCarousel>
@@ -63,3 +75,22 @@ const responsive: ResponsiveType = {
       items: 1,
    },
 };
+
+const responsives: ResponsiveType = {
+   2: {
+      breakpoint: { max: 3000, min: 0 },
+      items: 1,
+   },
+};
+
+const locations = [
+   { location: "Town, State", photo: { before: icons.properties["property1"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property2"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property3"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property1"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property2"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property3"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property1"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property2"], after: icons.properties["property1"] } },
+   { location: "Town, State", photo: { before: icons.properties["property3"], after: icons.properties["property1"] } },
+];
