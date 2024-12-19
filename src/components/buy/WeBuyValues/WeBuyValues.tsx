cb4ba49a70e457eb27a22c9abb5bd5868285cb4a
KhanhNgocNe <khanhngoc04032000@gmail.com>
2024-12-20 01:00:04 +0700

    wip: we buy values

--- src/components/buy/WeBuyValues/WeBuyValues.tsx
+++ src/components/buy/WeBuyValues/WeBuyValues.tsx
@@ -9,17 +9,17 @@ export const WeBuyValues = () => {
          <h2>What type of properties do we buy?</h2>
          <div className={`${styles.content}`}>
             <div className={`${styles.descriptionC}`}>
-                  <span>
-                     We are mainly looking for properties that are single family - including duplexes and triplexes -
-                     that have 2-5 bedrooms in the Pittsburgh, Cleveland, and Indianapolis metropolitan areas.
-                  </span>
-                  <span>
-                     We consider purchasing homes in all conditions, from <b>“In need of TLC”</b> to{" "}
-                     <b>
-                        “Rough Around the Edges”. However, we typically avoid properties with significant structural
-                        issues.
-                     </b>
-                  </span>
+               <span>
+                  We are mainly looking for properties that are single family - including duplexes and triplexes - that
+                  have 2-5 bedrooms in the Pittsburgh, Cleveland, and Indianapolis metropolitan areas.
+               </span>
+               <span>
+                  We consider purchasing homes in all conditions, from <b>“In need of TLC”</b> to{" "}
+                  <b>
+                     “Rough Around the Edges”. However, we typically avoid properties with significant structural
+                     issues.
+                  </b>
+               </span>
             </div>
             <div className={`${styles.properties}`}>
                <div className={`${styles.type}`}>
@@ -37,12 +37,12 @@ export const WeBuyValues = () => {
                         <h1>2-5 </h1> <span>Bedrooms</span>
                      </div>
                      <div className={`${styles.bedroomImg}`}>
-                        <Image src={icons_we_buy.value.bedroom.trimEnd()} alt="" width={200} height={300} />
+                        <Image src={icons_we_buy.value.bedroom.trimEnd()} alt="" fill />
                      </div>
                   </div>
                   <div className={`${styles.locationC}`}>
                      <div className={`${styles.locationImg}`}>
-                        <Image src={icons_we_buy.map.Pittsburgh.trimEnd()} alt="" width={200} height={300} />
+                        <Image src={icons_we_buy.map.Pittsburgh.trimEnd()} alt="" fill />
                      </div>
                      <div className={`${styles.location}`}>
                         <div className={`${styles.item}`}>
