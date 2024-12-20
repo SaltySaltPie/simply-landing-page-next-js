9b8dd29f558618a1f80a20d7ab84ab6dc111f443
ImOcean <nguyenngocnhan2104@gmail.com>
2024-12-20 11:59:11 +0700

    wip: CTA

--- src/components/we/WeCTA/WeCTA.tsx
+++ src/components/we/WeCTA/WeCTA.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import styles from "./WeCTA.module.scss";
 import { icons_we } from "@/utils/icon";
 
@@ -8,23 +7,17 @@ export const WeCTA = () => {
       <div className={`${styles.contentC}`}>
          <div className={`${styles.content}`}>
             <div className={`${styles.titleC}`}>
-               {/* <h2>{CTAData.title}</h2>
-               <h4>{CTAData.subTitle}</h4> */}
                <h2>Ready to Sell?</h2>
-               <h4>Give us just 30 seconds and we&apos;ll give you a Simply Homes Instant Offer.</h4>
+               <span>Give us just 30 seconds and we&apos;ll give you a Simply Homes Instant Offer.</span>
             </div>
-            <Link href={""}>
+            <button>
                <h5>Get an Offer</h5>
-            </Link>
+            </button>
+
             <div className={`${styles.backgroundImg}`}>
                <Image src={icons_we.CTA.cta_bg.trimEnd()} alt="" fill />
             </div>
          </div>
       </div>
    );
-};
-
-const CTAData = {
-   title: "Ready to Sell?",
-   subTitle: "Give us just 30 seconds and we'll give you a Simply Homes Instant Offer.",
 };
